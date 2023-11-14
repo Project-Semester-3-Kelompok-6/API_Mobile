3e54354efa63abc4685167c32fd3fe2c50897d90
Ahmad Muhyiddin <muhyinuris197703@gmail.com>
2023-11-14 10:17:18 +0700

    buat api login post running well

--- koneksi.php
+++ koneksi.php
@@ -1,32 +1,12 @@
 <?php
-
 $hostName = "localhost";
 $userName = "root";
 $password = "";
 $dbName = "wm_hanaasri";
 
-$koneksi =  mysqli_connect($hostName, $userName, $password);
+$koneksi = mysqli_connect($hostName, $userName, $password, $dbName);
 
-if(!$koneksi){
-    echo "koneksi gagal";
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
-
-/*
- * Encapsulate koneksi
-class koneksi {
-private $host = "localhost";
-private $username = "root";
-private $password = "";
-private $dbname = "hanaasri";
-
-public function getKoneksi() {
-    try {
-        $koneksi = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->username, $this->password);
-        $koneksi->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $koneksi;
-} catch (PDOException $e) {
-        die("Koneksi database gagal: " . $e->getMessage());
-        }
-    }
-}
-*/
+?>
